3dd13142e0c2654eb0e8daa4bf477b01e9fa53ee
Jayakranthi <jayakranthi@gmail.com>
2019-08-12 14:29:18 -0700

    first commit

---
+++ common/webpack.config.js
@@ -0,0 +1,39 @@
+
+module.exports = {
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: {
+					loader: "babel-loader"
+				}
+			},
+			{
+				test: /\.(gif|png|jpe?g|svg|ico)$/i,
+				use: [
+					{
+						loader: 'file-loader',
+						options: {
+							outputPath: 'build/images',
+							name: '[name].[ext]'
+						},
+					}
+					
+				]
+			},
+			{
+				test: /\.css$/,
+				use: ["style-loader", "css-loader"]
+			},
+			{
+				test: /\.aspx$/i,
+				use: 'raw-loader',
+			},
+		]
+	},
+	output: {
+		path: __dirname + "/",
+		filename: "thejavascript.js"
+	}
+}
---
+++ webpostage/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+import CommonApp from 'common/src/CommonApp'
+
+export default class App extends Component {
+
+	render() {
+
+		return (
+			<div>		
+				<h1>Webpostage Application</h1>
+				<CommonApp/>
+			</div>
+		);
+	}
+}
